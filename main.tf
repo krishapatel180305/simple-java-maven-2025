7cef4ff3cf92ca6f39199e99fc0d1c180e37a15e
krishapatel180305 <krishapatel1803@gmail.com>
2025-06-13 17:30:18 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,9 +5,28 @@ provider "aws" {
 resource "aws_instance" "Tf-instance" {
   ami           = "ami-0f3f13f145e66a0a3"
   instance_type = "t2.micro"
+
+  tags = {
+    Name = "Jenkins-CI-CD-Instance"
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "sudo yum update -y",
+      "sudo yum install -y docker git java-11-openjdk",
+      "sudo systemctl start docker",
+      "sudo systemctl enable docker"
+    ]
+  }
+
+  connection {
+    type        = "ssh"
+    user        = "ec2-user"
+    private_key = file("~/.ssh/my-key.pem")
+    host        = self.public_ip
+  }
 }
 
-# Output the public IP of the instance
 output "instance_public_ip" {
   description = "The public IP address of the EC2 instance"
   value       = aws_instance.Tf-instance.public_ip
