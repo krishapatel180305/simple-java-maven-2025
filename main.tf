b45ee2f2b0905095af6a0585815df56bbccbd306
krishapatel180305 <krishapatel1803@gmail.com>
2025-06-13 16:26:52 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,3 +6,9 @@ resource "aws_instance" "Tf-instance" {
   ami           = "ami-0f3f13f145e66a0a3"
   instance_type = "t2.micro"
 }
+
+# Output the public IP of the instance
+output "instance_public_ip" {
+  description = "The public IP address of the EC2 instance"
+  value       = aws_instance.Tf-instance.public_ip
+}
