073a04d70f7bc6fa315043493f5a7368136fe3e2
krishapatel180305 <krishapatel1803@gmail.com>
2025-06-25 15:56:52 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,7 @@
 provider "aws" {
-  region = "us-east-1"
+  region     = "us-east-1"
+  access_key = "AKIAQMNKFN3LYR5O66VV"
+  secret_key = "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"
 }
 
 resource "aws_instance" "build-server" {
