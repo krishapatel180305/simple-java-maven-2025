77b7b858eefe67286e16dcd8ec0f53263c8ab2a7
krishapatel180305 <krishapatel1803@gmail.com>
2025-06-25 13:21:20 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,36 +1,32 @@
 provider "aws" {
-  region = "us-east-1"  # Change to your desired region
-  access_key = AKIAQMNKFN3L6OJW7EIQ
-  secret_key = lfTJQdhqsaLS1AmAzRrQdRSN4g46SGU58DRUcIZt
+  region = "ap-south-1"
 }
 
-resource "aws_key_pair" "generated_key" {
-  key_name   = "terraform-key"
-  public_key = tls_private_key.generated.public_key_openssh
-}
+resource "aws_instance" "build-server" {
+  ami                         = "ami-0f58b397bc5c1f2e8" # Amazon Linux 2 AMI
+  instance_type               = "t2.micro"
+  key_name                    = "n-jenkins" # This must exactly match the key name you created in AWS
+  associate_public_ip_address = true
 
-resource "tls_private_key" "generated" {
-  algorithm = "RSA"
-}
+  connection {
+    type        = "ssh"
+    user        = "ec2-user"
+    private_key = file("C:/Users/ADMIN/Downloads/n-jenkins.pem")
+    host        = self.public_ip
+  }
 
-resource "aws_instance" "jenkins_server" {
-  ami           = "ami-0947d2ba12ee1ff75"  # Amazon Linux 2 AMI (ensure it's correct for your region)
-  instance_type = "t2.micro"
-  key_name      = aws_key_pair.generated_key.key_name
-
-  user_data = <<-EOF
-    #!/bin/bash
-    sudo yum update -y
-    sudo yum install -y docker git
-    sudo systemctl enable docker && sudo systemctl start docker
-    sudo docker run -d -p 80:80 --name nginx-container nginx
-  EOF
+  provisioner "remote-exec" {
+    inline = [
+      "sudo yum update -y",
+      "sudo yum install -y git java-11-openjdk-devel maven docker",
+      "sudo usermod -aG docker ec2-user",
+      "sudo systemctl start docker",
+      "curl -O https://downloads.apache.org/tomcat/tomcat-9/v9.0.78/bin/apache-tomcat-9.0.78.tar.gz",
+      "sudo tar -xzf apache-tomcat-9.0.78.tar.gz -C /opt/"
+    ]
+  }
 
   tags = {
-    Name = "Terraform-Automated-Instance"
+    Name = "cicd-build-instance"
   }
-}
-
-output "instance_ip" {
-  value = aws_instance.jenkins_server.public_ip
 }
